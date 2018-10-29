9950b457a25a01dfb94846e5ae6b9aa731e3728b
anrob <iamjustfresh@gmail.com>
2018-10-29 19:08:07 -0400

    changed route file to offers

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   get 'legal/privacy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 resources "offers"
-root to: 'welcome#index'
+root to: 'offers#index'
 end
